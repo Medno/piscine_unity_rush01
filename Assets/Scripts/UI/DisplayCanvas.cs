ebe3c0219cdc204583e1c8a2d53836ab54726c4a
Philippe Chadenier <phil.chadenier@gmail.com>
2019-10-26 16:26:59 +0200

    Add Inventory, spell bar and starting mouse over item description

---
+++ Assets/Scripts/UI/DisplayCanvas.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayCanvas : MonoBehaviour
+{
+    public GameObject  panel;
+    public KeyCode  key;
+    private bool    isActive;
+
+    void Start() {
+        isActive = false;
+        panel.SetActive(false);
+    }
+    void Update()
+    {
+        if (!isActive && Input.GetKeyDown(key)) {
+            panel.SetActive(true);
+            isActive = true;
+            Debug.Log("true");
+        }
+        else if (Input.GetKeyDown(key)) {
+            panel.SetActive(false);
+            isActive = false;
+            Debug.Log("false");
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Popup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Popup : MonoBehaviour
+{
+    public GameObject   popUp;
+    void Start()
+    {
+
+    }
+    void OnMouseEnter() {
+        Debug.Log("Selecting item");
+        popUp.SetActive(true);
+        // popUp.rectTransform.anchoredPosition = Input.mousePosition;
+
+    }
+    void OnMouseExit() {
+        Debug.Log("Quitting selection item");
+        popUp.SetActive(false);
+    }
+    void Update()
+    {
+        if (popUp.activeSelf)
+            popUp.GetComponent<RectTransform>().anchoredPosition = Input.mousePosition;
+
+    }
+}
