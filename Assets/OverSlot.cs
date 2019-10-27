7e18f0a7a9ed025856f6286bd9ddaa989b0655cb
Remy CHOFFARDET <rchoffar@e3r2p5.42.fr>
2019-10-27 18:31:28 +0100

    overSlot inc

---
+++ Assets/OverSlot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OverSlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public bool isInsideBox = false;
+    public bool isEmpty = true;
+
+    public void OnPointerEnter(PointerEventData eventData)
+      {
+        isInsideBox = true;
+        if(!isEmpty) {
+            print("OnMouseEnter");
+        }
+      }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isInsideBox = false;
+        print("OnMouseExit");
+        if(!isEmpty) {
+        }
+    }
+}
---
+++ Assets/TalentManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalentManager : MonoBehaviour
+{
+    public List<GameObject> Talents;
+    public List<GameObject> Slots;
+    // Start is called before the first frame update
+    public GameObject talentSelected;
+    public GameObject slotSelected;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            if (talentSelected == null)
+                talentSelected = Talents.Find(talent => talent.GetComponent<OverItem>().isInsideBox == true);
+            else
+                slotSelected = Slots.Find(slot => slot.GetComponent<OverSlot>().isInsideBox == true);
+            if (talentSelected != null && slotSelected != null) {
+                talentSelected = null;
+                slotSelected = null;
+            }
+        }
+    }
+}
