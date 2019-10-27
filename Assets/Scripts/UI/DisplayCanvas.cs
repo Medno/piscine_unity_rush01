80701b3e6a20ca292b3425bb6aac2da850cc0119
Philippe Chadenier <phil.chadenier@gmail.com>
2019-10-27 17:53:30 +0100

    Add Prefab UI and start sound

--- Assets/Scripts/UI/DisplayCanvas.cs
+++ Assets/Scripts/UI/DisplayCanvas.cs
@@ -1,15 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
-public class DisplayCanvas : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class DisplayCanvas : MonoBehaviour
 {
-    // public GameObject  panel;
     public KeyCode  key;
     private bool    isActive;
     private Popup   popUp;
-	[SerializeField] private UserInput inputManager = null;
     private Canvas canvas;
 
     void Start() {
@@ -18,30 +15,18 @@ public class DisplayCanvas : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
         canvas.enabled = false;
         popUp = GameObject.FindGameObjectWithTag("PopUp").GetComponent<Popup>();
     }
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        inputManager.canControl = false;
-    }
 
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        inputManager.canControl = true;
-    }
     void Update()
     {
         if (!isActive && Input.GetKeyDown(key)) {
-            // panel.SetActive(true);
             canvas.enabled = true;
             isActive = true;
-            Debug.Log("Display canvas");
+            // Debug.Log("Display canvas");
         }
         else if (Input.GetKeyDown(key)) {
             canvas.enabled = false;
-            // panel.SetActive(false);
             isActive = false;
-            // if (popUp.win && popUp.win.activeSelf)
-            //     popUp.win.gameObject.SetActive(false);
-            Debug.Log("Display canvas");
+            // Debug.Log("Display canvas");
         }
     }
 }
